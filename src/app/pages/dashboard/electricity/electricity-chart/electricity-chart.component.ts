89149c3491a6b6727f7ad4662cadecf3454ee7fe
SangLM <toiyeuhoahoc.pro@gmail.com>
2021-06-26 17:06:37 +0700

    commit '

---
+++ src/app/pages/dashboard/electricity/electricity-chart/electricity-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-electricity-chart',
+  templateUrl: './electricity-chart.component.html',
+  styleUrls: ['./electricity-chart.component.scss']
+})
+export class ElectricityChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/dashboard/kitten/kitten.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-kitten',
+  templateUrl: './kitten.component.html',
+  styleUrls: ['./kitten.component.scss']
+})
+export class KittenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
